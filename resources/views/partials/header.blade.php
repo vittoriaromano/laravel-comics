f34cc3fe98072bcf1d703726b3fb022aea6cd754
vittoriaromano <vittoria5005@gmail.com>
2023-05-02 18:43:54 +0400

    prova

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <!-- LOGO -->
+    <div class="logo">
+        <img src="{{ Vite::asset('resourses/img/dc-logo.png')}}" alt="logo dc">
+    </div>
+    <!-- MENU -->
+    <nav>
+        <ul>
+            <li v-for="menu in menulist">
+                <a>Characters</a>
+                <a>Comics</a>
+                <a>MOVIES</a>
+                <a href=""></a>
+            </li>
+            
+        </ul>
+    </nav>
+    
+</header>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/characters', function () {
+    return view('home');
+});
+
+Route::get('/comics', function () {
+    return view('home');
+});
